2d04a23ebbc3a4266610b4a78d81dd45579df010
orv3 <ratya.g@protonmail.com>
2023-01-26 19:08:27 +0530

    Uploaded All Files

---
+++ README.md
@@ -0,0 +1,18 @@
+# A Short Guide to Countries (Country Guide Site)
+
+## Background Information
+
+Following the keploy API followship, we were taught the basics of HTML, CSS and Javascript along with solid foundational knowledge on API. For the day 3, we were given the task of developing a project that utilizes API integration to get some hands on practice.
+
+## Task / Objective
+
+Day 3: Web Development & API Integration 
+
+Develop a project using HTML, CSS and javascript that includes API integration.
+
+## About the project
+
+The project includes three files, an index html file for which a css stylesheet and a working js file support is included. When a valid input of a country's name is given in the search bar, the api would fetch the particular country's information and the information would be shown to the user.
+
+API used in the project is: [Rest Countries API](https://restcountries.com/)
+Reference for project: [Coding Artist](https://www.youtube.com/watch?v=QDCmQHO8F8Q&t=936s)# Country-Guide-Site
---
+++ working.js
@@ -0,0 +1,87 @@
+let searchButton = document.getElementById("search-button");
+let countryInput = document.getElementById("country-input");
+
+searchButton.addEventListener('click', () => {
+    let countryName = countryInput.value;
+    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    console.log(finalURL);
+    fetch(finalURL)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data[0]);
+            console.log(data[0].capital[0]);
+            console.log(data[0].flags.svg);
+            console.log(data[0].name.common);
+            console.log(data[0].continents[0]);
+            console.log(Object.keys(data[0].currencies)[0]);
+            console.log(data[0].continents[0]);
+            console.log(data[0].timezones[0]);
+            console.log(Object.values(data[0].languages)
+                .toString()
+                .split(",")
+                .join(","));
+            console.log(data[0].maps.googleMaps);
+
+            result.innerHTML = `
+            <img src="${data[0].flags.svg}" class="flag-img">
+            <h2> ${data[0].name.common} </h2> 
+
+
+             <div class="wrapper"> 
+               <div class="data-wrapper">
+                  <h4>Capital:  </h4>
+                 <span> ${data[0].capital[0]}</span>
+              </div>
+
+              <div class="wrapper"> 
+               <div class="data-wrapper">
+                  <h4>Continent:  </h4>
+                 <span> ${data[0].continents[0]}</span>
+              </div>
+
+              <div class="wrapper"> 
+               <div class="data-wrapper">
+                  <h4>Population:  </h4>
+                 <span> ${data[0].population}</sp an>
+              </div>
+
+              <div class="wrapper"> 
+               <div class="data-wrapper">
+                  <h4>Timezone:  </h4>
+                 <span> ${(data[0].timezones[0])}</sp an>
+              </div>
+
+              <div class="wrapper"> 
+               <div class="data-wrapper">
+                  <h4>Currency:  </h4>
+                 <span> ${data[0].currencies[Object.keys(data[0].currencies)].name} - ${Object.keys(data[0].currencies)[0]}</span>
+              </div>
+
+              <div class="wrapper"> 
+               <div class="data-wrapper">
+                  <h4>Common Languages:  </h4>
+                 <span> ${Object.values(data[0].languages)
+                    .toString()
+                    .split(",")
+                    .join(",")}</sp an>
+              </div>
+              
+              <div class="wrapper"> 
+               <div class="data-wrapper">
+                  <h4>Google Map Link:  </h4>
+                 <span> ${data[0].maps.googleMaps}</sp an>
+              </div>
+
+    `;
+
+        }).catch(() => {
+            if (countryName.length == 0) {
+                result.innerHTML = `<h3> The input field cannot be empty...</h3>`;
+            }
+            else {
+                result.innerHTML = `<h3> Please enter a valid country name </h3>`;
+            }
+
+        })
+
+});
